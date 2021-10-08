7009ee9391ba9ea6e9bd24364f20e3bbebb99d9a
Sachin lokesh <sachin.lokesh@privatecircle.co>
2021-10-08 20:39:00 +0530

    My First commit

---
+++ loop.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+
+#syntax
+
+# for loop
+
+# for var in v1 v2 v3 v4.... vn
+# do
+#   statements to be executed
+# done
+
+
+# while loop
+
+# while command/condition
+# do
+#   statements to be executed
+# done
+
+
+
+
+
